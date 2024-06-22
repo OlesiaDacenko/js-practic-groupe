6ad92111b4b589327ff1df67d33d50c3b27e8cc2
Tina-Sl <slobodenukvm@gmail.com>
2024-06-22 15:25:00 +0200

    task3-2

--- js/index.js
+++ js/index.js
@@ -166,3 +166,24 @@ const cities = Object.keys(concerts)
   .toSorted((a, b) => concerts[a] - concerts[b]);
 
 console.log(cities);
+
+//TODO:==============================================
+/**
+ *? Поверніть об'єкт, в якому вказано кількість тегів.
+ *? Очікуваний результат {js: 3, nodejs: 3, html: 2, css: 2, react: 2}
+ */
+const tweets = [
+  { id: "000", likes: 5, tags: ["js", "nodejs"] },
+  { id: "001", likes: 2, tags: ["html", "css"] },
+  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+  { id: "003", likes: 8, tags: ["css", "react"] },
+  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+];
+
+const newArr = tweets.flatMap((item) => item.tags);
+console.log(newArr);
+const result = newArr.reduce((acc, item) => {
+  console.log(acc);
+  return { ...acc, [item]: acc[item] ? acc[item] + 1 : 1 };
+}, {});
+console.log(result);
